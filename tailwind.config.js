b2290f6c833a5e14df4a33acc6235e534685e4d2
Prabhakar Dev <ayushashi11@gmail.com>
2021-11-08 13:45:30 +0530

    improved site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      blue: colors.blue,
+      orange: colors.orange,
+    }
   },
   variants: {
     extend: {},
